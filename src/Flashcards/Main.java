d5b03b1895a50719bd8135bd6915ec72c792f73d
Tanton Gibbs <Tanton.Gibbs@gmail.com>
2012-01-20 01:45:25 -0800

    removed debug break

--- src/Flashcards/Main.java
+++ src/Flashcards/Main.java
@@ -3,11 +3,21 @@ package Flashcards;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
+import org.json.JSONException;
 
 public class Main {
-	public static void main(String args[]) throws IOException
+	public static void main(String args[]) throws IOException, JSONException
 	{
-		QuestionReader reader = new QuestionReader("QuestionFile.txt");
+		boolean shouldFilter = false;
+		String filter = "";
+		if (args.length > 0)
+		{
+			shouldFilter = true;
+			filter = args[0];
+		}
+		JSONQuestionReader reader = new JSONQuestionReader("QuestionFile.json");
 		InputStreamReader converter = new InputStreamReader(System.in, "UTF-8");
 		BufferedReader in = new BufferedReader(converter);	
 		OutputStreamWriter oconv = new OutputStreamWriter(System.out, "UTF-8");
@@ -18,19 +28,28 @@ public class Main {
 		{
 			questions.add(qa);
 		}
+		
 		java.util.Collections.shuffle(questions);
+		
+		Stats answerStats = new Stats();
 		for (QuestionAugmentations augments : questions)
 		{
+			if (shouldFilter && !augments.GetTags().contains(filter))
+			{
+				continue;
+			}
 			out.write(augments.GetQuestion().GetQuestion() + ": \n");
 			out.flush();
 			String answer = in.readLine();
 			if (augments.GetQuestion().IsCorrect(answer))
 			{
+				answerStats.Add(augments, true);
 				out.write("Correct\n");
 				out.flush();
 			}
 			else
 			{
+				answerStats.Add(augments, false);
 				out.write("Incorrect\n");
 				out.write("You gave: " + answer);
 				out.write("\n");
@@ -39,5 +58,11 @@ public class Main {
 				out.flush();
 			}
 		}
+		out.write("You answered " + answerStats.GetCorrectAnswers() + " / " + answerStats.GetTotalQuestions() + " correctly.\n");
+		for (Map.Entry<String, Stats.Pair> entry : answerStats.GetTagInfo().entrySet())
+		{
+			out.write("For tag [" + entry.getKey() + "] you answered " + entry.getValue().second + " / " + entry.getValue().second + " correctly\n");
+		}
+		out.flush();
 	}
 }
